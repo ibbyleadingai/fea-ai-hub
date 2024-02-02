a0deaf0a2562765ad418a0cbe72d901a328cc99d
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-02 12:23:23 +0000

    comment out iframe code

--- src/components/JobAdvertWriter.tsx
+++ src/components/JobAdvertWriter.tsx
@@ -3,7 +3,7 @@ export default function JobAdvertWriter(){
     return(
         <div>
             <h2>Job Advert Writer</h2>
-            <iframe src="feajobadverts.azurewebsites.net" width="100%" height="100%" allow="fullscreen" title="FEA Job Advert Writer"></iframe>
+            {/* <iframe src="feajobadverts.azurewebsites.net" width="100%" height="100%" allow="fullscreen" title="FEA Job Advert Writer"></iframe> */}
         </div>
     )
 }
