a4db3972b48b3bc160556320001b529992d38cea
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-16 16:42:49 +0000

    Testing iframe alt

--- src/components/JobAdvertWriter.tsx
+++ src/components/JobAdvertWriter.tsx
@@ -3,10 +3,12 @@ export default function JobAdvertWriter(){
     return(
         <div>
             <h2>Job Advert Writer</h2>
-            {/* <iframe src="feajobadverts.azurewebsites.net" width="100%" height="100%" allow="fullscreen" title="FEA Job Advert Writer"></iframe> */}
-            <iframe src="https://laisandbox.azurewebsites.net/"
+            {/* <iframe src="https://laisandbox.azurewebsites.net/"
             width="980" height="600" allow="fullscreen" title="TRI AI Assistant"
-            sandbox="allow-same-origin allow-scripts"></iframe>
+            sandbox="allow-same-origin allow-scripts"></iframe> */}
+            <object data="https://laisandbox.azurewebsites.net/" width="980" height="400">
+            <embed src="https://laisandbox.azurewebsites.net/" width="980" height="400"></embed>
+            </object>
         </div>
     )
 }
