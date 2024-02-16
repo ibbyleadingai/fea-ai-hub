1a5efcc27393439c4faa40fc1e3bc916ca473f66
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-16 09:54:00 +0000

    Added sandbox as iframe for testing

--- src/components/JobAdvertWriter.tsx
+++ src/components/JobAdvertWriter.tsx
@@ -4,6 +4,8 @@ export default function JobAdvertWriter(){
         <div>
             <h2>Job Advert Writer</h2>
             {/* <iframe src="feajobadverts.azurewebsites.net" width="100%" height="100%" allow="fullscreen" title="FEA Job Advert Writer"></iframe> */}
+            <iframe src="https://laisandbox.azurewebsites.net/"
+            width="100%" height="100%" allow="fullscreen" title="TRI AI Assistant"></iframe>
         </div>
     )
 }
