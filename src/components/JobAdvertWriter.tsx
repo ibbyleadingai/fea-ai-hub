2397e4ba864b6e823400fca7712c7fc7ac7308b6
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-16 09:56:37 +0000

    Update JobAdvertWriter.tsx

--- src/components/JobAdvertWriter.tsx
+++ src/components/JobAdvertWriter.tsx
@@ -5,7 +5,7 @@ export default function JobAdvertWriter(){
             <h2>Job Advert Writer</h2>
             {/* <iframe src="feajobadverts.azurewebsites.net" width="100%" height="100%" allow="fullscreen" title="FEA Job Advert Writer"></iframe> */}
             <iframe src="https://laisandbox.azurewebsites.net/"
-            width="100%" height="100%" allow="fullscreen" title="TRI AI Assistant"></iframe>
+            width="980" height="600" allow="fullscreen" title="TRI AI Assistant"></iframe>
         </div>
     )
 }
