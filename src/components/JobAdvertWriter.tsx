e8b272d9057154df3d3c9820632dcea48c30b0d8
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-16 16:52:25 +0000

    Added tab index attr to see if it fixes scrolling

--- src/components/JobAdvertWriter.tsx
+++ src/components/JobAdvertWriter.tsx
@@ -3,12 +3,12 @@ export default function JobAdvertWriter(){
     return(
         <div>
             <h2>Job Advert Writer</h2>
-            {/* <iframe src="https://laisandbox.azurewebsites.net/"
+            <iframe src="https://laisandbox.azurewebsites.net/"
             width="980" height="600" allow="fullscreen" title="TRI AI Assistant"
-            sandbox="allow-same-origin allow-scripts"></iframe> */}
-            <object data="https://laisandbox.azurewebsites.net/" width="980" height="600">
+            sandbox="allow-same-origin allow-scripts" tabIndex={-1}></iframe>
+            {/* <object data="https://laisandbox.azurewebsites.net/" width="980" height="600">
             <embed src="https://laisandbox.azurewebsites.net/" width="980" height="600"></embed>
-            </object>
+            </object> */}
         </div>
     )
 }
