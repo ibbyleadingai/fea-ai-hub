f3274bb537bebe0c3384668e99bc5ddb66d345ff
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2024-02-22 16:21:50 +0000

    Changed from iframe to embed object

--- src/components/JobAdvertWriter.tsx
+++ src/components/JobAdvertWriter.tsx
@@ -3,10 +3,13 @@ export default function JobAdvertWriter(){
     window.scrollTo = function () {};
     return(
         <div>
-            <h2>Job Advert Writer</h2>
+            {/* <h2>Job Advert Writer</h2>
             <iframe src="https://feajobadverts.azurewebsites.net/"
             width="980" height="600" allow="fullscreen" title="TRI AI Assistant"
-            sandbox="allow-same-origin allow-scripts"></iframe>
+            sandbox="allow-same-origin allow-scripts"></iframe> */}
+            <object data="https://feajobadverts.azurewebsites.net/" width="980" height="600">
+            <embed src="https://feajobadverts.azurewebsites.net/" width="980" height="600"></embed>
+            </object>
         </div>
     )
 }
